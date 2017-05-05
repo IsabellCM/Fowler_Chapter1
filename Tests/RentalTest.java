a9d2198d5f403d784101de71a0d8cdaaa2598172
IsabellCM <isabell@muszynski.de>
2017-05-05 16:32:12 +0200

    Update tests (Rental)

--- Tests/RentalTest.java
+++ Tests/RentalTest.java
@@ -4,25 +4,31 @@ import static org.junit.Assert.*;
  * Created by Isabell on 03.05.2017.
  */
 public class RentalTest {
+    public static final int DAYS_RENTED = 5;
+    public static final double PRICE = 6.5;
+    Rental testObject;
+    Movie movie;
+
     @org.junit.Before
     public void setUp() throws Exception {
-        Movie movie = new Movie("Titel", 1);
-Rental testObject=new Rental(movie,5);
+        movie = new Movie("Titel", Movie.REGULAR);
+        testObject=new Rental(movie, DAYS_RENTED);
+
     }
 
     @org.junit.Test
     public void getDaysRented() throws Exception {
-
+        assertEquals(DAYS_RENTED,testObject.getDaysRented());
     }
 
     @org.junit.Test
     public void getMovie() throws Exception {
-
+assertSame(movie,testObject.getMovie());
     }
 
     @org.junit.Test
     public void amountFor() throws Exception {
-
+assertEquals(PRICE,testObject.amountFor(),0.0);
     }
 
 }
